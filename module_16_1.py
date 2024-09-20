4545732078ff33bbdadef58b25a4254d477838fb
YaEd71 <166940263+YaEd71@users.noreply.github.com>
2024-09-20 11:13:15 +0300

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Главная страница"}
+
+@app.get("/user/admin")
+async def admin():
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/user/{user_id}")
+async def user(user_id: int):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+@app.get("/user")
+async def user_info(username: str, age: Optional[int] = None):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age if age else 'не указан'}"}
+
